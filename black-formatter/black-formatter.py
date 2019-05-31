cc66628606d3811f2ac8d67b0e9e529147109a77
itiievsk <itiyevskiy@gmail.com>
2019-05-31 15:44:04 +0300

    init

---
+++ black-formatter/black-formatter.py
@@ -0,0 +1,20 @@
+""" pre-commit check; determines if forbidden words are about to be checked into the repo. """
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import os
+import sys
+
+
+def main():
+    """ entry point for the module"""
+
+    print("Hello from Black!")
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
