34243b01b9f1419a3efbcf16636128ac95e74c3e
itiievsk <itiyevskiy@gmail.com>
2019-05-31 15:58:12 +0300

    setup files

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name='black-formatter',
-    description='no naughty words in the repo',
-    url='https://github.com/itiievskyi/black-pre-commit',
-    version='1.0.0',
-    author='itiievkyi',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    packages=find_packages(exclude=('tests*', 'testing*')),
-    entry_points={
-        'console_scripts': [
-            'black-formatter = black-formatter.black-formatter:main',
-        ],
-    },
-)
+setup()
